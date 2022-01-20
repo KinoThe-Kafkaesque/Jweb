317dedf84bc90ed6a0daeb003e17d07cf0b27a9f
KinoThe-Kafkaesque <abdellahkadem5@gmail.com>
2022-01-20 13:43:39 +0100

    please i beg

--- src/main/java/domain/Email.java
+++ src/main/java/domain/Email.java
@@ -2,13 +2,13 @@ package domain;
 
 import java.util.Properties;
 
-import jakarta.mail.Message;
-import jakarta.mail.MessagingException;
-import jakarta.mail.PasswordAuthentication;
-import jakarta.mail.Session;
-import jakarta.mail.Transport;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class Email {
 
@@ -22,7 +22,7 @@ public class Email {
 		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.port", "465");
-		Session session = Session.getDefaultInstance(props, new jakarta.mail.Authenticator() {
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication( username, password);
